8f0cfea0a6329c19d658ce86a333f5e4eda700c4
Pablo Gascón <pablogasconiel445@gmail.com>
2025-02-23 13:24:24 -0300

    adding GET users requisitions support

--- user-service/src/main/java/com/pablo/user_service/controllers/UserController.java
+++ user-service/src/main/java/com/pablo/user_service/controllers/UserController.java
@@ -1,7 +1,33 @@
 package com.pablo.user_service.controllers;
 
+import com.pablo.user_service.entities.UserEntity;
+import com.pablo.user_service.services.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@RequestMapping("/users")
 @RestController
+@AllArgsConstructor
 public class UserController {
+    private final UserService userService;
+
+    @GetMapping("/me")
+    public ResponseEntity<UserEntity> authenticatedUser(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserEntity currentUser = (UserEntity) authentication.getPrincipal();
+        return ResponseEntity.ok(currentUser);
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<UserEntity>> allUsers(){
+        List<UserEntity> users = userService.allUsers();
+        return ResponseEntity.ok(users);
+    }
 }
--- user-service/src/main/java/com/pablo/user_service/services/UserService.java
+++ user-service/src/main/java/com/pablo/user_service/services/UserService.java
@@ -1,7 +1,18 @@
 package com.pablo.user_service.services;
 
+import com.pablo.user_service.entities.UserEntity;
+import com.pablo.user_service.repositories.UserRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@AllArgsConstructor
 public class UserService {
+    private final UserRepository userRepository;
+
+    public List<UserEntity> allUsers() {
+        return userRepository.findAll();
+    }
 }
